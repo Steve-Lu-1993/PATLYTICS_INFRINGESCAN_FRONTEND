d4763df1c6f15d2596055cb86792f63793ca4a81
steve-lu-1993 <ilikelgsr@gmail.com>
2024-11-11 17:14:18 +0800

    fix:build_error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "./AppSideBar";
+import { AppSidebar } from "./AppSidebar";
 import { useGenericQuery } from "@/hooks/useGenericQuery";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
